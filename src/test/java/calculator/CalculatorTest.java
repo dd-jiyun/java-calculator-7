39e1c1c374acfec754bbe593f6e4215aa4c5db26
dd-jiyun <dlwldbs8015@gmail.com>
2025-03-25 14:48:15 +0900

    test: 덧셈 계산 기능 테스트

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,29 @@
+package calculator;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    @DisplayName("리스트의 값을 더한다.")
+    void shouldReturnAdd_whenValues() {
+        Calculator calculator = new Calculator();
+        int result = calculator.addValues(List.of(1, 2, 3, 4, 5));
+
+        assertEquals(15, result);
+    }
+
+    @Test
+    @DisplayName("리스트에 값이 없을 때 0을 반환한다.")
+    void shouldReturnZero_whenEmptyValue() {
+        Calculator calculator = new Calculator();
+        int result = calculator.addValues(List.of());
+
+        assertEquals(0, result);
+    }
+}
