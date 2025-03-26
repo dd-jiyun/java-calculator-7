2c9b9fd292c65b98fdb9c3d34e3c6dc8d03519b5
dd-jiyun <dlwldbs8015@gmail.com>
2025-03-26 14:33:16 +0900

    test: 변환 기능 예외 처리 테스트

--- src/test/java/calculator/ParserTest.java
+++ src/test/java/calculator/ParserTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
