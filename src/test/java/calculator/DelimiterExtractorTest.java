e3fc33ff81c86f61ab6c61204f3520ebe565ccb2
dd-jiyun <dlwldbs8015@gmail.com>
2025-03-27 16:07:28 +0900

    test: 구분자 추출 기능 테스트
    - 구분자를 추출하여 사용할 수 있는 구분자인지 검증합니다.

--- src/test/java/calculator/DelimiterExtractorTest.java
+++ src/test/java/calculator/DelimiterExtractorTest.java
@@ -1,11 +1,13 @@
 package calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class DelimiterExtractorTest {
 
@@ -25,5 +27,20 @@ class DelimiterExtractorTest {
 
         //then
         assertEquals(List.of(expected.split("")), result);
+    }
+
+    @ParameterizedTest
+    @DisplayName("기본 구분자에 속하지 않는 구분자를 사용할 경우 예외가 발생한다.")
+    @ValueSource(strings = {"1,2;3", "1;2;3","1:2#3"})
+    void shouldReturnEmptyList_whenNotDefaultDelimiter(String input) {
+        //given
+        DelimiterExtractor extractor = new DelimiterExtractor();
+
+        //when
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> extractor.extract(input));
+
+        //then
+        assertEquals("기본 구분자(,나 :)를 사용해주세요.", exception.getMessage());
     }
 }
