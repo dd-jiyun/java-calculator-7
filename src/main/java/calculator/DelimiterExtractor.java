6bf86de81750c925ad83dc9ae70a11f9283ed635
dd-jiyun <dlwldbs8015@gmail.com>
2025-03-27 16:08:29 +0900

    refactor: 구분자 추출 기능 수정
    - 추출된 구분자가 사용할 수 있는 구분자인지 확인하는 기능을 추가하였습니다.
    - 구분자가 사용할 수 없는 구분자일 경우 예외를 발생시키도록 수정하였습니다.

--- src/main/java/calculator/DelimiterExtractor.java
+++ src/main/java/calculator/DelimiterExtractor.java
@@ -1,13 +1,45 @@
 package calculator;
 
+import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Set;
 
 public class DelimiterExtractor {
-    public List<String> extract(String input) {
-        return Stream.of(Delimiter.values())
-                .map(Delimiter::getDelimiter)
-                .filter(input::contains)
-                .toList();
+
+    public List<String> extract(final String input) {
+        Set<String> delimiterSet = getDelimiterSet(input);
+        List<String> delimiters = List.copyOf(delimiterSet);
+
+        validateDefaultDelimiters(delimiters);
+        return delimiters;
+    }
+
+    private Set<String> getDelimiterSet(final String input) {
+        Set<String> delimiterSet = new LinkedHashSet<>();
+
+        for (char c : input.toCharArray()) {
+            processCharacter(c, delimiterSet);
+        }
+
+        return delimiterSet;
+    }
+
+    private void validateDefaultDelimiters(final List<String> delimiters) {
+        for (String delimiter : delimiters) {
+            validateDelimiter(delimiter);
+        }
+    }
+
+    private void processCharacter(char c, Set<String> delimiterSet) {
+        if (Character.isDigit(c)) {
+            return;
+        }
+        delimiterSet.add(String.valueOf(c));
+    }
+
+    private void validateDelimiter(final String delimiter) {
+        if (!Delimiter.isValidDelimiter(delimiter)) {
+            throw new IllegalArgumentException("기본 구분자(,나 :)를 사용해주세요.");
+        }
     }
 }
