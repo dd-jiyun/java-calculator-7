b8a4054907e553f858a2310d84ba6dc655b411bc
dd-jiyun <dlwldbs8015@gmail.com>
2025-03-28 18:11:30 +0900

    feat: 문자열 구분하는 기능 구현
    - 전달받은 구분자를 통해 입력받은 문자열을 구분하는 기능입니다.

---
+++ src/main/java/calculator/CustomDelimiterFormat.java
@@ -0,0 +1,6 @@
+package calculator;
+
+public class CustomDelimiterFormat {
+    public static final String CUSTOM_DELIMITER_PREFIX = "//";
+    public static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+}
---
+++ src/main/java/calculator/StringSplitter.java
@@ -0,0 +1,34 @@
+package calculator;
+
+import static calculator.CustomDelimiterFormat.CUSTOM_DELIMITER_PREFIX;
+import static calculator.CustomDelimiterFormat.CUSTOM_DELIMITER_SUFFIX;
+
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class StringSplitter {
+
+    public List<String> split(String input, final List<String> delimiters) {
+        input = preprocessingCustomDelimiter(input);
+
+        String regex = getRegex(delimiters);
+        String[] splitResult = input.split(regex);
+
+        return List.of(splitResult);
+    }
+
+    private String preprocessingCustomDelimiter(String input) {
+        if (input.startsWith(CUSTOM_DELIMITER_PREFIX) && input.contains(CUSTOM_DELIMITER_SUFFIX)) {
+            input = input.substring(input.indexOf(CUSTOM_DELIMITER_SUFFIX) + 2);
+        }
+
+        return input;
+    }
+
+    private String getRegex(final List<String> delimiters) {
+        return delimiters.stream()
+                .map(Pattern::quote)
+                .collect(Collectors.joining("|"));
+    }
+}
