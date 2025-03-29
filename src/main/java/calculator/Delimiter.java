e6b1ba7aa52cf98f6c216da223e2a517b4a6abe7
dd-jiyun <dlwldbs8015@gmail.com>
2025-03-29 18:18:31 +0900

    refactor: 커스텀 구분자 검증 기능 구현
    - 커스텀 구분자로 사용할 수 없는 문자열을 명시하여 검증합니다.

--- src/main/java/calculator/Delimiter.java
+++ src/main/java/calculator/Delimiter.java
@@ -3,18 +3,31 @@ package calculator;
 import java.util.stream.Stream;
 
 public enum Delimiter {
-    COMMA(","),
-    COLON(":");
+    COMMA(",", true),
+    COLON(":", true),
+    DOUBLE_SLASH("//", false),
+    NEW_LINE("\\n", false),
+    NUMBER("[0-9]+", false),
+    SPACE(" ", false)
+    ;
 
     private final String delimiter;
+    private final boolean isDefault;
 
-    Delimiter(final String delimiter) {
+    Delimiter(final String delimiter, final boolean isDefault) {
         this.delimiter = delimiter;
+        this.isDefault = isDefault;
     }
 
     public static boolean isValidDelimiter(String delimiter) {
         return Stream.of(values())
+                .filter(d -> d.isDefault)
                 .anyMatch(d -> d.getDelimiter().equals(delimiter));
+    }
+
+    public static boolean isValidCustomDelimiter(String delimiter) {
+        return Stream.of(values())
+                .noneMatch(d -> d.getDelimiter().equals(delimiter)) && !delimiter.matches(NUMBER.getDelimiter());
     }
 
     public String getDelimiter() {
