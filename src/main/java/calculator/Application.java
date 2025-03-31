4852356eeb4c579cccd92747a4a58977c5a02d7f
dd-jiyun <dlwldbs8015@gmail.com>
2025-03-31 14:44:21 +0900

    refactor: 실행 흐름 관리 controller 추가
    - 기존에는 main에서 실행과 흐름 제어를 담당하고 있었는데, controller가 흐름 제어를 담당하도록 변경하였습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,29 +1,18 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
 import calculator.model.Calculator;
 import calculator.model.DelimiterExtractor;
-import calculator.model.Parser;
 import calculator.model.StringSplitter;
-import calculator.view.InputView;
-import calculator.view.OutputView;
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
 
-        String input = InputView.getInput();
-
-        DelimiterExtractor delimiterExtractor = new DelimiterExtractor();
-        List<String> delimiters = delimiterExtractor.extract(input);
-
-        StringSplitter stringSplitter = new StringSplitter();
-        List<String> splitResult = stringSplitter.split(input, delimiters);
-
-        List<Integer> parseResult = Parser.parse(splitResult);
-
-        Calculator calculator = new Calculator();
-        int result = calculator.add(parseResult);
-
-        OutputView.printResult(result);
+        CalculatorController calculatorController = new CalculatorController(
+                new DelimiterExtractor(),
+                new StringSplitter(),
+                new Calculator()
+        );
+        calculatorController.run();
     }
 }
---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,35 @@
+package calculator.controller;
+
+import calculator.model.Calculator;
+import calculator.model.DelimiterExtractor;
+import calculator.model.Parser;
+import calculator.model.StringSplitter;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+import java.util.List;
+
+public class CalculatorController {
+
+    private final DelimiterExtractor delimiterExtractor;
+    private final StringSplitter stringSplitter;
+    private final Calculator calculator;
+
+    public CalculatorController(final DelimiterExtractor delimiterExtractor,
+                                final StringSplitter stringSplitter,
+                                final Calculator calculator) {
+        this.delimiterExtractor = delimiterExtractor;
+        this.stringSplitter = stringSplitter;
+        this.calculator = calculator;
+    }
+
+    public void run() {
+        String input = InputView.getInput();
+
+        List<String> delimiters = delimiterExtractor.extract(input);
+        List<String> numbers = stringSplitter.split(input, delimiters);
+        List<Integer> parsedNumbers = Parser.parse(numbers);
+        int result = calculator.add(parsedNumbers);
+
+        OutputView.printResult(result);
+    }
+}
